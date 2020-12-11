b6e18ad100c9f6c40baa155e0a970b6471ae24e9
danygarciamartin10@gmail.com <danygarciamartin10@gmail.com>
2020-12-11 15:53:41 +0000

    metricas supuestamente añadidas

--- src/main/java/io/swagger/api/impl/server.java
+++ src/main/java/io/swagger/api/impl/server.java
@@ -6,6 +6,9 @@ import io.swagger.model.TomaTemperatura;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.net.Socket;
+import java.io.DataOutputStream;
+
 public class server{
     
     private List<pairUserTemp> userTempList;
@@ -24,6 +27,29 @@ public class server{
     }
 
     public void setUserTemp(String user, TomaTemperatura temp){
+        Socket conn = null;
+        DataOutputStream dos = null;
+        
+            try {
+                if(temp.getTemperatura() > 37 && temp.getPulso() > 100){
+                    conn = new Socket("da2ae0dd.carbon.hostedgraphite.com", 2003);
+                    dos = new DataOutputStream(conn.getOutputStream());
+                    dos.writeBytes("a17d2a80-1dc6-4372-8edb-29a65eeec5ba.sla.errorAlerta 1\n");
+                    if(conn != null && !conn.isClosed()){
+                        conn.close();
+                    }
+                }else{
+                    conn = new Socket("da2ae0dd.carbon.hostedgraphite.com", 2003);
+                    dos = new DataOutputStream(conn.getOutputStream());
+                    dos.writeBytes("a17d2a80-1dc6-4372-8edb-29a65eeec5ba.sla.noErrorAlerta 0\n");
+                    if(conn != null && !conn.isClosed()){
+                        conn.close();
+                    }
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        
         System.out.println("SendTemp.java - User de para: " + user + " User de temp : " + temp.getIdUser() + " temp: " + temp.getTemperatura() + " pulso: " + temp.getPulso() + " timestamp " + temp.getTimestamp());
         boolean found = false;
         for (int i = 0; i <userTempList.size() && !found; i++) {
